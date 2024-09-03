ab31f93c3ecc756f0c20f60a705c6cc4eb113796
zap8600 <95487746+zap8600@users.noreply.github.com>
2024-09-03 13:45:00 -0500

    fix

--- main.c
+++ main.c
@@ -20,8 +20,8 @@ int main()
 		CNFGClearFrame();
 		CNFGGetDimensions( &w, &h );
 
-		for(int xZ = 0; x < w; x++) {
-            for(int yZ = 0; y < h; y++) {
+		for(int xZ = 0; xZ < w; x++) {
+            for(int yZ = 0; yZ < h; y++) {
                 int x = 0;
                 int y = 0;
 
